cb27b31009c86efba757545a6c1e21e33be7c69c
Aditya Jaiswar <adityajaiswar11@gmail.com>
2024-04-03 05:38:50 -0800

    added search functionality

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,20 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react"
-import { CountryCard } from "./CountryCard"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import { CountryCard } from "./CountryCard";
+import axios from "axios";
 
 export const CountryList = ({ search }) => {
-  const [country, setCountry] = useState([])
-  const [filterCountry, setFilterCountry] = useState([])
+  const [country, setCountry] = useState([]);
+  const [filterCountry, setFilterCountry] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await axios.get("https://restcountries.com/v3.1/all")
+      const res = await axios.get("https://restcountries.com/v3.1/all");
       const data = res.data;
-      setCountry(data)
-    }
-    fetchData()
-  }, [])
+      setCountry(data);
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const filteredCountries = country.filter((data) => {
@@ -27,17 +27,23 @@ export const CountryList = ({ search }) => {
   return (
     <>
       <main className="w-[85%] mx-auto px-5 mt-10">
-        <h1 className="font-medium text-2xl opacity-90 first-letter:text-red-600 text-center">Country Details</h1>
-        {filterCountry.length>0 ? (
-           <div className="mt-[3rem] mb-5 grid grid-cols-3 gap-5">
-           {filterCountry.map((data) => <CountryCard data={data} key={data.id} />)}
-         </div>
-        ):(
+        <h1 className="font-medium text-2xl opacity-90 first-letter:text-red-600 text-center">
+          Country Details
+        </h1>
+        {filterCountry.length > 0 ? (
+          <div className="mt-[3rem] mb-5 grid md:grid-cols-3 gap-6 grid-cols-1 items-center">
+            {filterCountry.map((data) => (
+              <CountryCard data={data} key={data.id} />
+            ))}
+          </div>
+        ) : (
           <>
-          <h1 className="text-center mt-[5rem] text-2xl font-semibold">No Country Found</h1>
+            <h1 className="text-center mt-[5rem] text-2xl font-semibold">
+              No Country Found
+            </h1>
           </>
         )}
       </main>
     </>
-  )
-}
+  );
+};
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 
-
-export const Searchbar = ({search,setSearch}) => {
+export const Searchbar = ({ search, setSearch }) => {
   return (
     <>
       <main className="w-full py-10">
@@ -10,17 +9,19 @@ export const Searchbar = ({search,setSearch}) => {
             <input
               type="text"
               value={search}
-              onChange={(e)=>setSearch(e.target.value)}
+              onChange={(e) => setSearch(e.target.value)}
               placeholder="Search a country"
               className="w-[300px] h-10 outline-none border border-gray-300 px-2 py-2 hover:border-black/70 rounded-sm"
             />
           </div>
           <div>
             <select className="w-[200px] border border-black py-2 px-2 rounded-sm">
-              <option value="india">India</option>
-              <option value="india">India</option>
-              <option value="india">India</option>
-              <option value="india">India</option>
+                    <option value="0">Select Region</option>
+              <option value="asia">Asia</option>
+              <option value="africa">Africa</option>
+              <option value="Americas"> Americas</option>
+              <option value="Europe">Europe</option>
+              <option value="Oceania">Oceania</option>
             </select>
           </div>
         </div>
